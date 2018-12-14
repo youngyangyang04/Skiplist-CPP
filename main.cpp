20eb28f90fa3a4a9248e64307f54a05419b33055
youngyangyang04 <826123027@qq.com>
2018-12-14 11:46:07 +0800

    Update

--- main.cpp
+++ main.cpp
@@ -12,26 +12,28 @@
 int main() {
     // 在这里为什么要手工传，因为不用类型的 k，v 初始化的参数是不一样的。例如string，就不能把
     // 这里还是要改成 初始化 三个参数，要不然去header 上的值容易崩， 也不用改，内部分装好。不回取
-    SkipList<int, std::string> skipList(6);
-	skipList.insertElement(3, "a"); 
-	skipList.insertElement(6, "b"); 
-	skipList.insertElement(7, "c"); 
-	skipList.insertElement(9, "sun"); 
-	skipList.insertElement(12, "xiu"); 
-	skipList.insertElement(19, "yang"); 
+    SkipList<std::string, std::string> skipList(6);
+	skipList.insertElement("1", "a"); 
+	skipList.insertElement("3", "b"); 
+	skipList.insertElement("7", "c"); 
+	skipList.insertElement("8", "sun"); 
+	skipList.insertElement("9", "xiu"); 
+	skipList.insertElement("19", "yang"); 
+
+    skipList.displayList();
 
     skipList.dumpFile();
 
-    std::string path = "./store/dumpFile";
-    skipList.loadFile(path);
+    // std::string path = "./store/dumpFile";
+    // skipList.loadFile(path);
 
-    skipList.searchElement(9);
-    skipList.searchElement(18);
+    // skipList.searchElement("9");
+    // skipList.searchElement("18");
 
 
-    skipList.displayList();
+    // skipList.displayList();
 
-    skipList.deleteElement(3);
+    // skipList.deleteElement("3");
 
-    skipList.displayList();
+    // skipList.displayList();
 }
