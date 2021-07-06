592878918bfed78917cb38878d34a17cfa26b70a
youngyangyang04 <826123027@qq.com>
2021-07-06 15:10:27 +0800

    更新键值为int

--- main.cpp
+++ main.cpp
@@ -11,14 +11,15 @@
 
 int main() {
 
-    SkipList<std::string, std::string> skipList(6);
-	skipList.insert_element("1", "学"); 
-	skipList.insert_element("3", "算法"); 
-	skipList.insert_element("7", "认准"); 
-	skipList.insert_element("8", "微信公众号：代码随想录"); 
-	skipList.insert_element("9", "学习"); 
-	skipList.insert_element("19", "算法不迷路"); 
-	skipList.insert_element("19", "赶快关注吧你会发现详见很晚！"); 
+    // 键值中的key用int型，如果用其他类型，需要自定义比较函数
+    SkipList<int, std::string> skipList(6);
+	skipList.insert_element(1, "学"); 
+	skipList.insert_element(3, "算法"); 
+	skipList.insert_element(7, "认准"); 
+	skipList.insert_element(8, "微信公众号：代码随想录"); 
+	skipList.insert_element(9, "学习"); 
+	skipList.insert_element(19, "算法不迷路"); 
+	skipList.insert_element(19, "赶快关注吧你会发现详见很晚！"); 
 
     std::cout << "skipList size:" << skipList.size() << std::endl;
 
@@ -26,14 +27,14 @@ int main() {
 
     // skipList.load_file();
 
-    skipList.search_element("9");
-    skipList.search_element("18");
+    skipList.search_element(9);
+    skipList.search_element(18);
 
 
     skipList.display_list();
 
-    skipList.delete_element("3");
-    skipList.delete_element("7");
+    skipList.delete_element(3);
+    skipList.delete_element(7);
 
     std::cout << "skipList size:" << skipList.size() << std::endl;
 
